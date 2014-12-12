a7dca71b7e91473167ff885c3c64ea0f9918d2af
Matheus Avellar <Tetheu98@users.noreply.github.com>
2014-12-12 16:40:41 -0300

    Woops

--- AutoGrab.js
+++ AutoGrab.js
@@ -3,7 +3,7 @@ if(window.location.hostname === "plug.dj"){
 	API.chatLog('         :purple_heart:   AutoGrab enabled by default   :purple_heart:',false);
 	API.chatLog('Do /stop to disable AutoGrab and /start to reenable it',false);
 	API.on(API.CHAT_COMMAND, input);
-	API.on(API.DJ_ADVANCE, grab);
+	API.on(API.ADVANCE, grab);
 
 	grab();
 	var grablock = false;
