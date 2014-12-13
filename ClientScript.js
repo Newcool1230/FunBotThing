914ec918a2d0e1131c7302f3ee14796bea1a861f
Matheus Avellar <Tetheu98@users.noreply.github.com>
2014-12-13 11:23:18 -0300

    Added some Timeout

--- ClientScript.js
+++ ClientScript.js
@@ -378,7 +378,7 @@ if(window.location.hostname === "plug.dj"){
 
 			case "xp":
 				ct('There is a limit to how much XP you can earn in a single 24-hour period, which starts at midnight GMT. XP and PP are earned on ticks. There are a specific amount of ticks in a 24-hour period and you can earn XP');
-				ct('for a certain amount of them, and they do not need to be earned sequentially (you can earn 3 ticks now, 4 ticks later, etc.)');
+				setTimeout(function(){ct('for a certain amount of them, and they do not need to be earned sequentially (you can earn 3 ticks now, 4 ticks later, etc.)');},250);
 				break;
 
 			case "pp":
