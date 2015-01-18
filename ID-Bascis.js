a9165e8d223882ecd9ff8fa9e8b814817da120d5
Matheus Avellar <Tetheu98@users.noreply.github.com>
2015-01-17 22:21:23 -0300

    Test

--- ID-Bascis.js
+++ ID-Bascis.js
@@ -26,18 +26,26 @@ function lookfor(id){
 		url: 'https://plug.dj/_/users/' + id
 	}).done(function(user) {
 		data = user.data[0];
+	
+	if (data.username == null){
+		addChat("<b><a style='color:#eaaeae;'>[User has not updated yet!]</a></b>","#CCCCCC",false,false,true);
+	}else{
+
+//STATUS
 		switch (data.status){
-			case 0:	var stt = "Available (0)";break;
-			case 1:	var stt = "Away (1)";break;
-			case 2:	var stt = "Working (2)";break;
-			case 3:	var stt = "Gaming (3)";break;
-			case 4:	var stt = "Offline / Undefined (4?)";break;
-			default:var stt = "Wot.";
+			case 0:	var stt = "<a style='color:#89be6c;'>Available</a> (0)";break;
+			case 1:	var stt = "<a style='color:#ffdd6f;'>Away</a> (1)";break;
+			case 2:	var stt = "<a style='color:#f04f30;'>Working</a> (2)";break;
+			case 3:	var stt = "<a style='color:#ac76ff;'>Gaming</a> (3)";break;
+			case 4:	var stt = "<a style='color:#555d70;'>Offline / Undefined</a> (4?)";break;
+			default:var stt = "<a style='color:#ff0000;'>Wot.</a>";
 		}
+
+//BADGE
 		switch(data.badge){
 			case "bt":			var bb = "Beta Tester (" + data.badge + ")";break;
 			case "ss":			var bb = "Plug SuperStar (" + data.badge + ")";break;
-			case "og":			var bb = "Original Ganster (" + data.badge + ")";break;
+			case "og":			var bb = "Original Gangster (" + data.badge + ")";break;
 			case "ea":			var bb = "Early Adopter (" + data.badge + ")";break;
 			case "ba":			var bb = "Brand Ambassador (" + data.badge + ")";break;
 			case "admin":		var bb = "Admin Badge (" + data.badge + ")";break;
@@ -66,6 +74,7 @@ function lookfor(id){
 			case "animals01":	var bb = "Wolf Badge (" + data.badge + ")";break;
 			case "animals02":	var bb = "Cat Badge (" + data.badge + ")";break;
 			case "animals03":	var bb = "Chicken Badge (" + data.badge + ")";break;
+			case "animals04":	var bb = "Boxer Badge (" + data.badge + ")";break;
 			case "style01":		var bb = "Shoe Badge (" + data.badge + ")";break;
 			case "style02":		var bb = "Joystick Badge (" + data.badge + ")";break;
 			case "tiki01":		var bb = "Fat Tiki Mask (" + data.badge + ")";break;
@@ -80,6 +89,8 @@ function lookfor(id){
 				}
 				break;
 		}
+
+//JOINED
 		var jin = data.joined.split('-');
 		var lk = jin[2].split(' ');
 		var lj = lk[1].split('.');
@@ -106,48 +117,72 @@ function lookfor(id){
 		}
 		var jnd = mnt + " " + lk[0] + day +  " " + jin[0] + " at " + lj[0];
 
-		if (data.gRole < 3){var g = "Regular (" + data.gRole + ")";};
-		if (data.gRole == 3){var g = "Brand Ambassador (" + data.gRole + ")";};
-		if (data.gRole > 3){var g = "Admin (" + data.gRole + ")";};
+//ROLE
+		if (data.gRole < 3){var g = "<a style='color:#777f92;'>Regular</a> (" + data.gRole + ")";};
+		if (data.gRole == 3){var g = "<a style='color:#89be6c;'>Brand Ambassador</a> (" + data.gRole + ")";};
+		if (data.gRole > 3){var g = "<a style='color:#42a5dc;'>Admin</a> (" + data.gRole + ")";};
 
-		//** Supposed to be working but isn't */
-		var votestats = 'None';
-		var votestate = 0;
+//VOTE
+		var userInfo;
+		var votestats = "<a style='color:#646b7e;'>Not in the room</a>";
+		var grabstats = "";
+		var votestate;
+		var grabstate;
 		for (var i = 0; i < API.getUsers().length; i++){
 			if (API.getUsers()[i].username == data.username){
 				votestate = API.getUsers()[i].vote;
+				grabstate = API.getUsers()[i].grab;
+				userInfo = API.getUsers()[i];
 			}
 		}
-		if (votestate == 1){votestats == "Woot!"}
-		else if (votestate == 0){votestats == "None"}
-		else if (votestate == -1){votestats == "Meh"}
-		//** Supposed to be working but isn't */
+		if (votestate == 1){votestats = "<a style='color:#90ad2f;'>Woot!</a> (1) "}
+		else if (votestate == 0){votestats = "<a style='color:#646b7e;'>Didn't vote</a> (0) "}
+		else if (votestate == -1){votestats = "<a style='color:#c42e3b;'>Meh</a> (-1) "}
+		if (grabstate === true){grabstats = "| <a style='color:#aa74ff;'>Grabbed!</a> (<em>true</em>)"}
+		else if (grabstate === false){grabstats = " <a style='color:#646b7e;'>| Didn't grab</a> (<em>false</em>)"}
 
+		if (API.getDJ().username == data.username){
+			votestats = "<a style='color:#646b7e;'>(is currently DJ'ing)</a>";
+			grabstats = "";
+		}
+
+		var itsYou = false;
+		if (typeof userInfo != "undefined" && userInfo.username == API.getUser().username){itsYou = true;};
+
+//BLURB
 		var blurbTrue = "<a style='color:#eaaeae;'>[None]</a>";
 		if (data.blurb != null){
 			blurbTrue = data.blurb;
 		}
+//FRIEND
+		var isFriend = "";
+		if (typeof userInfo != "undefined" && userInfo.friend == true){
+			isFriend = "<a style='color:#ffc4f9;'>Yes</a> (<em>true</em>)";
+		}else{
+			isFriend = "No (<em>false</em>)";
+		}
+		if (itsYou){isFriend = "<a style='color:#646b7e;'>You can't be friends with yourself</a> (<em>" + userInfo.friend + "</em>)";}
 
+//PROFILE
 		var hasProfile = "<a style='color:#eaaeae;'>[No profile yet]</a>";
-		var profileColor = "#eaaeae"
+		var profileColor = "#eaaeae";
 		if (data.level > 5){
 			hasProfile = "";
-			profileColor = "#aec9ea"
+			profileColor = "#aec9ea";
 		}
 
-		if (data.username == null){
-			addChat("<b><a style='color:#eaaeae;'>[User has not updated yet!]</a></b>","#CCCCCC");
-		}else{
-			addChat("<b>Name:</b> " + data.username + "<br><b>\
-			Blurb:</b> " + blurbTrue + "<br><b>\
-			ID:</b> " + data.id + "<br><b>\
-			Level:</b> " + data.level + "<br><b>\
-			Avatar:</b> " + data.avatarID + "<br><b>\
-			Status:</b> " + stt + "<br><b>\
-			Role:</b> " + g + "<br><b>\
-			Joined:</b> " + jnd + "<br><b>\
-			Badge:</b> " + bb + "<br><b>\
-			Slug:</b> <a style='color: " + profileColor + ";' href='/@/" + data.slug + "' target='_blank'>" + data.slug + "</a> " + hasProfile, "#CCCCCC");
+		addChat("<br><a style='color:#42a5dc;'><b>Name:</b></a> " + data.username + "<br><b>\
+		<a style='color:#42a5dc;'>Slug:</b></a> <a style='color: " + profileColor + ";' href='/@/" + data.slug + "' target='_blank'>" + data.slug + "</a> " + hasProfile + "<br><b>\
+		<a style='color:#42a5dc;'>Blurb:</b></a> " + blurbTrue + "<br><b>\
+		<a style='color:#42a5dc;'>ID:</b></a> " + data.id + "<br><b>\
+		<a style='color:#42a5dc;'>Level:</b></a> " + data.level + "<br><b>\
+		<a style='color:#42a5dc;'>Avatar:</b></a> " + data.avatarID + "<br><b>\
+		<a style='color:#42a5dc;'>Status:</b></a> " + stt + "<br><b>\
+		<a style='color:#42a5dc;'>Role:</b></a> " + g + "<br><b>\
+		<a style='color:#42a5dc;'>Joined:</b></a> " + jnd + "<br><b>\
+		<a style='color:#42a5dc;'>Badge:</b></a> " + bb + "<br><b>\
+		<a style='color:#42a5dc;'>Friend:</b></a> " + isFriend + "<br><b>\
+		<a style='color:#42a5dc;'>Vote:</b></a> " + votestats + grabstats,"#CCCCCC",false,false,true);
 		}
 	});
 }
@@ -194,8 +229,11 @@ API.on(API.CHAT_COMMAND, function(data){
 		case "sing":
 			c("/me ♪ " + command[1] + " ♫");
 			break;
-
+			
 		case "getid":
+		case "getuid":
+		case "id":
+		case "uid":
 			var xname = command[1].substring(1).toString();
 			var oname = xname.substring(0,xname.length - 2);
 			var uname = oname.toLowerCase();
@@ -220,6 +258,7 @@ API.on(API.CHAT_COMMAND, function(data){
 			break;
 
 		case "cmd":
+		case "cmds":
 			addChat("<a style='color:#eb9595;'>__________</a><br>\
 			<a style='color:#ececec;'>/<b>getid</b> + [<b>@username </b>]</a><br>\
 			<a style='color:#c8c8c8;'>Returns user's ID (if user is in the room)</a><br><br>\
@@ -241,31 +280,36 @@ API.on(API.CHAT_COMMAND, function(data){
 });
 
 
-function addChat(text, color, state, size) {
-	var chat = $('#chat-messages'),
-		a = chat.scrollTop() > chat[0].scrollHeight - chat.height() - 28;
+function addChat(text, color, state, hasBottom, isNotCenter) {
+	var chat = $('#chat-messages');
+	var a = chat.scrollTop() > chat[0].scrollHeight - chat.height() - 28;
 
-	if (color == undefined)
-		color = "#9fee00";
-		
-	if (state){
-		chat.append("<div class='update leidscript' style='border-left: double 6px " + color + "';'><center><span class='leidscript-text' style='color: " + color + "; " + si + "'>" + text + "<br></span></center></div>");
-	}else{
-		chat.append("<div class='update leidscript' style=''><center><span class='leidscript-text' style='color: " + color + "; " + si + "'>" + text + "<br></span></center></div>");
+	if (color == undefined){
+		color = "#99ffd7";
 	}
 
-	if (size){
-		var si = "font-size: 11px;";
+	if (isNotCenter){
+		chat.append("<div class='update betabot-update' style='background-color:#0a0a0a;'><div class='text-margin' style='margin-left: 10px;'><span class='betabot-text' style='color: " + color + "; font-size: 12px;'>" + text + "<br></span></div></div>");
 	}else{
-		var si = "";
+		if (hasBottom){
+			chat.append("<div class='update betabot-update' style='background-color:#0a0a0a; border-left: double 6px " + color + "; border-bottom: double 6px " + color + "'><center><span class='betabot-text' style='color: " + color + "; font-size: 13px;'>" + text + "<br></span></center></div>");
+		}else{
+			if (state){
+				chat.append("<div class='update betabot-update' style='background-color:#0a0a0a; border-left: double 6px " + color + "; margin-top:5px;margin-bottom:5px;'><center><span class='betabot-text' style='color: " + color + "; font-size: 12px;'>" + text + "<br></span></center></div>");
+			}else{
+				chat.append("<div class='update betabot-update' style='background-color:#0a0a0a; margin-top:5px;margin-bottom:5px;'><center><span class='betabot-text' style='color: " + color + ";'>" + text + "<br></span></center></div>");
+			}
+		}
 	}
 
-	if (a)
-		chat.scrollTop(chat[0].scrollHeight);
-	if (chat.children().length >= 512)
+	if (a){
+		$('#chat-messages').scrollTop(50000);
+	}
+	if (chat.children().length >= 512){
 		chat.children().first().remove();
+	}
 }
 
-addChat("<a style='color:#ececec;'>Beta Tester's getID script initiated!</a><br>\
-	<a style='color:#eb9595;'>Beta v0.6</a><br>\
+addChat("<a style='color:#ececec;'>Beta Tester's <a style='color:#42a5dc;'>getID</a> script initiated!</a><br>\
+	<a style='color:#eb9595;'>Beta v0.6.1</a><br>\
 	<a style='color:#ececec;'>Check commands with /cmd</a>","#CCCCCC");
